a705ea528dd796221a1609d380f321ff593f6d82
Annyn Matheson <annynm@gmail.com>
2024-05-17 13:41:53 -0700

    Fix merge conflicts

---
+++ frontend_js/nav.js
@@ -0,0 +1,29 @@
+document.addEventListener("click", e => {
+  const isDropdownButton = e.target.matches("[data-dropdown-button]")
+  if (!isDropdownButton && e.target.closest("[data-dropdown]") != null) return
+
+  let currentDropdown
+  if (isDropdownButton) {
+    currentDropdown = e.target.closest("[data-dropdown]")
+    currentDropdown.classList.toggle("active")
+  }
+
+  document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+    if (dropdown === currentDropdown) return
+    dropdown.classList.remove("active")
+  })
+})
+
+function activateUser() {
+  document.getElementById('goferLinks').style.display = 'none';
+  document.getElementById('userLinks').style.display = 'flex';
+  document.getElementById('goferMobile').style.display = 'none';
+  document.getElementById('userMobile').style.display = 'flex';
+}
+
+function activateGofer() {
+  document.getElementById('userLinks').style.display = 'none';
+  document.getElementById('goferLinks').style.display = 'flex';
+  document.getElementById('userMobile').style.display = 'none';
+  document.getElementById('goferMobile').style.display = 'flex';
+} 
